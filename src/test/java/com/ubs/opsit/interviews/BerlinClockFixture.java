35326560db36994f372be307599ff4e9c8d2b757
Peter Suggitt <me@suggs.org.uk>
2013-12-12 11:31:34 +0000

    Added berlin clock

---
+++ src/test/java/com/ubs/opsit/interviews/BerlinClockFixture.java
@@ -0,0 +1,38 @@
+package com.ubs.opsit.interviews;
+
+import org.jbehave.core.annotations.Then;
+import org.jbehave.core.annotations.When;
+import org.junit.Test;
+
+import static com.ubs.opsit.interviews.support.BehaviouralTestEmbedder.aBehaviouralTestRunner;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Acceptance test class that uses the JBehave (Gerkin) syntax for writing stories.  You should not need to
+ * edit this class to complete the exercise, this is your definition of done.
+ */
+public class BerlinClockFixture {
+
+    private TimeConverter berlinClock;
+    private String theTime;
+
+    @Test
+    public void berlinClockAcceptanceTests() throws Exception {
+        aBehaviouralTestRunner()
+                .usingStepsFrom(this)
+                .withStory("berlin-clock.story")
+                .run();
+    }
+
+    @When("the time is $time")
+    public void whenTheTimeIs(String time) {
+        theTime = time;
+    }
+
+    @Then("the clock should look like $")
+    public void thenTheClockShouldLookLike(String theExpectedBerlinClockOutput) {
+        assertThat(berlinClock.convertTime(theTime), is(equalTo(theExpectedBerlinClockOutput)));
+    }
+}
